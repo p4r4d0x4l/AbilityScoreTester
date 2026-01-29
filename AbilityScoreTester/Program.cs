d785f92182087c712d9323ace4684e13b3f896e6
p4r4d0x4l <263694@usn.no>
2026-01-29 13:25:03 +0100

    Add project files.

---
+++ AbilityScoreTester/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbilityScoreTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "\nStarting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "\nDivide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "\nAdd amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "\nMinimum");
+
+                calculator.CalculateAbilityScore();
+
+                Console.WriteLine("\n\nCalculated ability score: " + calculator.Score);
+                Console.WriteLine("\n\nPress Q to quit, any other key to continue");
+
+                char keyChar = Console.ReadKey(true).KeyChar;
+
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine("\n    using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("\n    using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine("\n    using value " + value);
+                return value;
+            } else
+            {
+                Console.WriteLine("\n    using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
